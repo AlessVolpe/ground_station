e7c4ca1af21b775b12df5bb1ff05d7e255469a21
Alessandro Volpe <37452470+Syro98@users.noreply.github.com>
2020-05-19 18:39:35 +0200

    wip

--- esempio.php
+++ esempio.php
@@ -1,8 +1,20 @@
 <?php
-$connetion = mysqli_connect("localhost","root","");
-mysqli_select_db($connetion, 'ground_station');
-$velocity = $_GET ['velocity'];
-$coords = $_GET ['coords'];
-$mode = $_GET ['mode'];
+if(isset($_POST['submit'])) {
+    $velocity = $_GET ['velocity'];
+    $coords = $_GET ['coords'];
+    $mode = $_GET ['mode'];
+    
+    $connetion = mysqli_connect("localhost","root","");
+    mysqli_select_db($connetion, 'ground_station');
 
+    $query = "INSERT into unnamed(velocity, coords, mode) VALUES ('$velocity', '$coords', '$mode')";
+    $result = mysqli_query($connetion, $query);
+    
+    if($result) {
+        echo "Dati inseriti";
+    }
+    else {
+        echo "Invalid";
+    }
+}
 ?>
