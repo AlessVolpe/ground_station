ec7c0f5f2aedd7fda889fe25e2854f54e368e439
DebFarruggio <farruggio.deborah@gmail.com>
2020-05-27 22:36:45 +0200

    update

--- php/query.php
+++ php/query.php
@@ -1,54 +1,61 @@
 <?php
-if (isset($_POST['subtodb'])) {
-    $roverID = $_POST['roverID'];
-    $speed = $_POST['speed'];
-    $coordsx = $_POST['coordsx'];
-    $coordsy = $_POST['coordsy'];
-    $cameraTilt = $_POST['cameraTilt'];
-    $mode = $_POST['mode'];
+session_start();
+if($_SESSION['userLogin'] == 'loggato'){
+	if (isset($_POST['subtodb'])) {
+		$roverID = $_POST['roverID'];
+		$speed = $_POST['speed'];
+		$coordsx = $_POST['coordsx'];
+		$coordsy = $_POST['coordsy'];
+		$cameraTilt = $_POST['cameraTilt'];
+		$mode = $_POST['mode'];
 
-    $connetion = mysqli_connect("localhost", "root", "");
-    mysqli_select_db($connetion, 'sasa');
+		$connetion = mysqli_connect("localhost", "root", "");
+		mysqli_select_db($connetion, 'sasa');
 
-    $query = "INSERT into rover(roverID, speed, coordsx, coordsy, cameraTilt, mode) VALUES ('$roverID', '$speed', '$coordsx',  '$coordsy',  '$cameraTilt', '$mode')";
-    $result = mysqli_query($connetion, $query);
+		$query = "INSERT into rover(roverID, speed, coordsx, coordsy, cameraTilt, mode) VALUES ('$roverID', '$speed', '$coordsx',  '$coordsy',  '$cameraTilt', '$mode')";
+		$result = mysqli_query($connetion, $query);
 
-    if ($result) {
-        header("Location: ../unnamed.html#realTime");
-    } else {
-        echo "Error";
-    }
+		if ($result) {
+			header("Location: ../unnamed.html#realTime");
+		} else {
+			echo "Error";
+		}
+	}
+
+	else if (isset($_POST['start'])) {
+		$statusStart = $_POST['statusStart'];
+
+		$connetion = mysqli_connect("localhost", "root", "");
+		mysqli_select_db($connetion, 'sasa');
+
+		$query = "INSERT into rover(roverID, status) VALUES ('1', '$statusStart')";
+		$result = mysqli_query($connetion, $query);
+
+		if ($result) {
+			header("Location: ../unnamed.html#realTime");
+		} else {
+			echo "Error";
+		}
+	}
+
+	else if (isset($_POST['stop'])) {
+		$statusStop = $_POST['statusStop'];
+
+		$connetion = mysqli_connect("localhost", "root", "");
+		mysqli_select_db($connetion, 'sasa');
+
+		$query = "INSERT into rover(roverID, status) VALUES ('1', '$statusStop')";
+		$result = mysqli_query($connetion, $query);
+
+		if ($result) {
+			header("Location: ../unnamed.html#realTime");
+		} else {
+			echo "Error";
+		}
+	}
 }
 
-else if (isset($_POST['start'])) {
-    $statusStart = $_POST['statusStart'];
-
-    $connetion = mysqli_connect("localhost", "root", "");
-    mysqli_select_db($connetion, 'sasa');
-
-    $query = "INSERT into rover(roverID, status) VALUES ('1', '$statusStart')";
-    $result = mysqli_query($connetion, $query);
-
-    if ($result) {
-        header("Location: ../unnamed.html#realTime");
-    } else {
-        echo "Error";
-    }
-}
-
-else if (isset($_POST['stop'])) {
-    $statusStop = $_POST['statusStop'];
-
-    $connetion = mysqli_connect("localhost", "root", "");
-    mysqli_select_db($connetion, 'sasa');
-
-    $query = "INSERT into rover(roverID, status) VALUES ('1', '$statusStop')";
-    $result = mysqli_query($connetion, $query);
-
-    if ($result) {
-        header("Location: ../unnamed.html#realTime");
-    } else {
-        echo "Error";
-    }
+else{
+	header("Location: login.html");
 }
 ?>
--- php/roverAuto.php
+++ php/roverAuto.php
@@ -1,22 +1,28 @@
 <?php
-$roverID = $_POST['roverID'];
-$roverStatus = $_POST['roverStatus'];
-$battery = $_POST['battery'];
-$speed = $_POST['speed'];
-$coordsx = $_POST['coordsx'];
-$coordsy = $_POST['coordsy'];
-$cameraTilt = $_POST['cameraTilt'];
-$mode = $_POST['mode'];
 
-$connetion = mysqli_connect("localhost", "root", "");
-mysqli_select_db($connetion, 'sasa');
+session_start();
+if($_SESSION['userLogin'] == 'loggato'){
+	$roverID = $_POST['roverID'];
+	$roverStatus = $_POST['roverStatus'];
+	$battery = $_POST['battery'];
+	$speed = $_POST['speed'];
+	$coordsx = $_POST['coordsx'];
+	$coordsy = $_POST['coordsy'];
+	$cameraTilt = $_POST['cameraTilt'];
+	$mode = $_POST['mode'];
 
-$query = "INSERT into rover(roverID, roverStatus, battery, speed, coordsx, coordsy, cameraTilt, mode) VALUES ('$roverID', '$roverStatus', '$battery', $speed', '$coordsx',  '$coordsy',  '$cameraTilt', '$mode')";
-$result = mysqli_query($connetion, $query);
+	$connetion = mysqli_connect("localhost", "root", "");
+	mysqli_select_db($connetion, 'sasa');
 
-if ($result) {
-    echo"yes";
-} else {
-    echo "Error";
+	$query = "INSERT into rover(roverID, roverStatus, battery, speed, coordsx, coordsy, cameraTilt, mode) VALUES ('$roverID', '$roverStatus', '$battery', $speed', '$coordsx',  '$coordsy',  '$cameraTilt', '$mode')";
+	$result = mysqli_query($connetion, $query);
+
+	if ($result) {
+		echo"yes";
+	} else {
+		echo "Error";
+	}
+else{
+	header("Location: login.html");
 }
 ?>
