548cd5bbe35984d1802cecc765368921e62a36e6
DebFarruggio <farruggio.deborah@gmail.com>
2020-05-28 21:02:24 +0200

    update

--- php/logout.php
+++ php/logout.php
@@ -2,7 +2,7 @@
 	session_start();
 	session_unset();
 	session_destroy();
-	setcookie ("ID_my_site", "", time() - 3600);
+	//setcookie ("ID_my_site", "", time() - 3600);
 	header('location: ../login.html');
 	exit();
 ?>
