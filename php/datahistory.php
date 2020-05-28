e5c1a6feba5347bf2200cd88e54900782e0a2893
DebFarruggio <farruggio.deborah@gmail.com>
2020-05-28 13:03:33 +0200

    Update datahistory.php

--- php/datahistory.php
+++ php/datahistory.php
@@ -1,71 +1,73 @@
 <?php
 
 session_start();
-	
-	// inizio codice che killa la sessione dopo cinque minuti di inattività
-	$time = $_SERVER['REQUEST_TIME'];
-	$timeout_duration = 300;
-	if (isset($_SESSION['LAST_ACTIVITY']) && ($time - $_SESSION['LAST_ACTIVITY']) > $timeout_duration) {
-		session_unset();
-		session_destroy();
-		session_start();
-	}
-
-	$_SESSION['LAST_ACTIVITY'] = $time;
-	
-	// fine
-	
-	// controllo se sono loggato
 
 	if($_SESSION['userLogin'] == 'loggato'){
 		$connetion = mysqli_connect("localhost", "root", "") or die(mysqli_error($connetion));
 		mysqli_select_db($connetion, "sasa") or die(mysqli_error($connetion));
+		
+		if(isset($_REQUEST['invia'])){
+			$date = $_POST['date'];
+			echo $date;
+			$query = mysqli_query($connetion, "SELECT * FROM rover WHERE CAST(timestamp as DATE) = '$date' AND roverID=1 ORDER BY id DESC");
+			echo "<br><br>
+			<center><table border='1'>
+			<tr>
+			<th>Timestamp</th>
+			<th>Status</th>
+			<th>Battery</th>
+			<th>Speed</th>
+			<th>Coords x</th>
+			<th>Coords y</th>
+			<th>Camera Tilt</th>
+			<th>Mode</th>
 
-
-		$query = mysqli_query($connetion, "SELECT * FROM rover WHERE roverID=1");
-		echo "<center><table border='1'>
-		<tr>
-		<th>Timestamp</th>
-		<th>Status</th>
-		<th>Battery</th>
-		<th>Speed</th>
-		<th>Coords x</th>
-		<th>Coords y</th>
-		<th>Camera Tilt</th>
-		<th>Mode</th>
-
-		</tr>";
-		while ($row = mysqli_fetch_array($query)) {
-			echo '<h4 style="color:#fff; text-align:center;">';
-			echo "<tr>";
-			echo "<th>".$row['timestamp']."</th>";
-			echo "<th>".$row['status']."</th>";
-			echo "<th>".$row['battery']."</th>";
-			echo "<th>".$row['speed']."</th>";
-			echo "<th>".$row['coordsx']."</th>";
-			echo "<th>".$row['coordsy']."</th>";
-			echo "<th>".$row['cameraTilt']."</th>";
-			echo "<th>".$row['mode']."</th>";
-			echo '</tr>';
-
-			/*echo "Rover ID: ";
-			echo $row['roverID'];
-			echo "<br>Speed: ";
-			echo $row['speed'];
-			echo "<br>Coords x: ";
-			echo $row['coordsx'];
-			echo "<br>Coords y: ";
-			echo $row['coordsy'];
-			echo "<br>Camera Tilt: ";
-			echo $row['cameraTilt'];
-			echo "<br>Mode: ";
-			echo $row['mode'];
-			echo "<br>Timestamp: ";
-			echo $row['timestamp'];
-			echo "<br><br>";
-			echo '</h4>';*/
+			</tr>";
+			while ($row = mysqli_fetch_array($query)) {
+				echo '<h4 style="color:#fff; text-align:center;">';
+				echo "<tr>";
+				echo "<th>".$row['timestamp']."</th>";
+				echo "<th>".$row['status']."</th>";
+				echo "<th>".$row['battery']."</th>";
+				echo "<th>".$row['speed']."</th>";
+				echo "<th>".$row['coordsx']."</th>";
+				echo "<th>".$row['coordsy']."</th>";
+				echo "<th>".$row['cameraTilt']."</th>";
+				echo "<th>".$row['mode']."</th>";
+				echo '</tr>';
+			}
+			echo "</table></center>";
 		}
-		echo "</table></center>";
+	
+		else{
+			$query = mysqli_query($connetion, "SELECT * FROM rover WHERE roverID=1 ORDER BY id DESC LIMIT 5");
+			echo "<center><table border='1'>
+			<tr>
+			<th>Timestamp</th>
+			<th>Status</th>
+			<th>Battery</th>
+			<th>Speed</th>
+			<th>Coords x</th>
+			<th>Coords y</th>
+			<th>Camera Tilt</th>
+			<th>Mode</th>
+
+			</tr>";
+			while ($row = mysqli_fetch_array($query)) {
+				echo '<h4 style="color:#fff; text-align:center;">';
+				echo "<tr>";
+				echo "<th>".$row['timestamp']."</th>";
+				echo "<th>".$row['status']."</th>";
+				echo "<th>".$row['battery']."</th>";
+				echo "<th>".$row['speed']."</th>";
+				echo "<th>".$row['coordsx']."</th>";
+				echo "<th>".$row['coordsy']."</th>";
+				echo "<th>".$row['cameraTilt']."</th>";
+				echo "<th>".$row['mode']."</th>";
+				echo '</tr>';
+			}
+			echo "</table></center>";
+		}
 	}
 	
 	else{
