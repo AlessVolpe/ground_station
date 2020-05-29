582ee21e03742c1daf228914ac7001fc7ceb69b0
Giovanni Roma <r.giovanni.98@gmail.com>
2020-05-29 09:25:56 +0200

    Added SQL script

---
+++ sasa.sql
@@ -0,0 +1,121 @@
+-- phpMyAdmin SQL Dump
+-- version 5.0.2
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Creato il: Mag 29, 2020 alle 09:09
+-- Versione del server: 10.4.11-MariaDB
+-- Versione PHP: 7.4.5
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `sasa`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Struttura della tabella `ground_station`
+--
+
+CREATE TABLE `ground_station` (
+  `id` int(3) NOT NULL,
+  `roverID` int(3) NOT NULL,
+  `speed` double NOT NULL,
+  `coordsx` varchar(255) NOT NULL,
+  `coordsy` varchar(255) NOT NULL,
+  `cameraTilt` int(3) NOT NULL,
+  `mode` varchar(50) NOT NULL,
+  `timestamp` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Struttura della tabella `rover`
+--
+
+CREATE TABLE `rover` (
+  `id` int(255) NOT NULL,
+  `roverID` int(3) NOT NULL,
+  `status` varchar(10) NOT NULL,
+  `battery` int(3) NOT NULL,
+  `speed` float NOT NULL,
+  `coordsx` varchar(12) NOT NULL,
+  `coordsy` varchar(12) NOT NULL,
+  `cameraTilt` int(3) NOT NULL,
+  `mode` varchar(255) NOT NULL,
+  `timestamp` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Struttura della tabella `users`
+--
+
+CREATE TABLE `users` (
+  `username` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `email` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dump dei dati per la tabella `users`
+--
+
+INSERT INTO `users` (`username`, `password`, `email`) VALUES
+('sasa', 'sttAdmin', 'sasa@sasa');
+
+--
+-- Indici per le tabelle scaricate
+--
+
+--
+-- Indici per le tabelle `ground_station`
+--
+ALTER TABLE `ground_station`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indici per le tabelle `rover`
+--
+ALTER TABLE `rover`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indici per le tabelle `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`username`),
+  ADD UNIQUE KEY `email` (`email`);
+
+--
+-- AUTO_INCREMENT per le tabelle scaricate
+--
+
+--
+-- AUTO_INCREMENT per la tabella `ground_station`
+--
+ALTER TABLE `ground_station`
+  MODIFY `id` int(3) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=20;
+
+--
+-- AUTO_INCREMENT per la tabella `rover`
+--
+ALTER TABLE `rover`
+  MODIFY `id` int(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=150;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
