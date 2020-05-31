dda7290aa17fcd343f8611b03936c2ff9404436d
DebFarruggio <farruggio.deborah@gmail.com>
2020-05-31 23:44:50 +0200

    Update roverAeneas.php

--- rovers/roverAeneas.php
+++ rovers/roverAeneas.php
@@ -10,14 +10,15 @@
 <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=no" />
 
 <head>
-  <title>Hector</title>
+  <title>Aeneas</title>
   <!--inline CSS styling-->
   <style>
     body {
       overflow-y: hidden;
     }
+  
   </style>
-    <!--JavaScript function when the rover is turned on, set the corresponding dot to green, 
+  <!--JavaScript function when the rover is turned on, set the corresponding dot to green, 
       remove the blinking animation, set the rover status to Idle, and restore the commands
       div in the Ground Station-->
   <script>
@@ -25,12 +26,12 @@
       document.getElementsByClassName('on').disabled = true;
       document.getElementsByClassName('stop').disabled = false;
       document.getElementsByClassName('off').disabled = false;
-      window.localStorage.removeItem('dot03');
-      window.localStorage.setItem('dot03', 'green');
-      window.localStorage.setItem('dot03blink', 'no');
-      window.localStorage.setItem('rover03status', 'Idle');
-      window.localStorage.removeItem('commands03');
-      document.getElementById('rover03status').innerText = window.localStorage.getItem('rover03status');
+      window.localStorage.removeItem('dot02');
+      window.localStorage.setItem('dot02', 'green');
+      window.localStorage.setItem('dot02blink', 'no');
+      window.localStorage.setItem('rover02status', 'Idle');
+      window.localStorage.removeItem('commands02');
+      document.getElementById('rover02status').innerText = window.localStorage.getItem('rover02status');
     }
   </script>
   <!--JavaScript function when the rover is stopped, set the rover status to Idle, 
@@ -40,12 +41,12 @@
       document.getElementsByClassName('on').disabled = true;
       document.getElementsByClassName('stop').disabled = false;
       document.getElementsByClassName('off').disabled = false;
-      window.localStorage.removeItem('dot03');
-      window.localStorage.setItem('dot03', 'green');
-      window.localStorage.setItem('dot03blink', 'no');
-      window.localStorage.setItem('rover03status', 'Idle');
-      window.localStorage.removeItem('commands03');
-      document.getElementById('rover03status').innerText = window.localStorage.getItem('rover03status');
+      window.localStorage.removeItem('dot02');
+      window.localStorage.setItem('dot02', 'green');
+      window.localStorage.setItem('dot02blink', 'no');
+      window.localStorage.setItem('rover02status', 'Idle');
+      window.localStorage.removeItem('commands02');
+      document.getElementById('rover02status').innerText = window.localStorage.getItem('rover02status');
       window.localStorage.setItem('roverAuto', '0');
     }
   </script>
@@ -57,13 +58,13 @@
       document.getElementsByClassName('on').disabled = false;
       document.getElementsByClassName('off').disabled = true;
       document.getElementsByClassName('stop').disabled = true;
-      window.localStorage.removeItem('dot03');
-      window.localStorage.setItem('dot03', 'red');
-      window.localStorage.setItem('dot03blink', 'no');
-      window.localStorage.setItem('rover03status', 'Offline');
-      document.getElementById('rover03status').innerText = window.localStorage.getItem('rover03status');
-      window.localStorage.removeItem('commands03');
-      window.localStorage.setItem('commands03', 'none');
+      window.localStorage.removeItem('dot02');
+      window.localStorage.setItem('dot02', 'red');
+      window.localStorage.setItem('dot02blink', 'no');
+      window.localStorage.setItem('rover02status', 'Offline');
+      document.getElementById('rover02status').innerText = window.localStorage.getItem('rover02status');
+      window.localStorage.removeItem('commands02');
+      window.localStorage.setItem('commands02', 'none');
       window.localStorage.setItem('roverAuto', '0');
     }
   </script>
@@ -79,17 +80,17 @@
   $row = mysqli_fetch_array($query)
   ?>
   <!--here starts the actual page content-->
-    <!--last commands received from the Ground Station-->
+  <!--last commands received from the Ground Station-->
   <div class="container2">
     <h1>Ground Station data</h1>
     <div class="row">
       <div class="col-md-6">
         <br />
         <i style="margin-left: 4px;" class="fa fa-power-off mr-1"></i>
-        <!--if speed >0 Running, else Stopped or if not online Power Off-->
+        <!--if speed > 0 Running, else Stopped or if not online Power Off-->
         <label class="contact-form__label" for="velocity">Rover status:
         </label>
-        <span style="margin-left: 10px;" id="rover03status" name="roverStatus"></span>
+        <span style="margin-left: 10px;" id="rover02status" name="roverStatus"></span>
         <br />
         <i class="fa fa-battery-three-quarters mr-1"></i>
         <label class="contact-form__label" for="velocity">Battery level:
@@ -109,14 +110,6 @@
         <i style="margin-left: 9px; visibility: hidden;" class="fa fa-map-marker-alt mr-1"></i>
         <label class="contact-form__label" for="velocity">Y-coords:</label>
         <span style="margin-left: 10px;" name="yData"><?php echo $row['coordsy']; ?></span>
-        <br />
-        <i style="margin-left: 7px;" class="fa fa-crop-alt mr-1"></i>
-        <label class="contact-form__label" for="velocity">Camera tilt:</label>
-        <span style="margin-left: 10px;" name="yData"><?php echo $row['cameraTilt']; ?>°</span>
-        <br />
-        <i style="margin-left: 5px;" class="fa fa-rocket mr-1"></i>
-        <label class="contact-form__label" for="velocity">Rover mode: </label>
-        <span style="margin-left: 10px;" name="modeData"><?php echo $row['mode']; ?></span>
         <br /><br /><br />
         <!--a form with buttons, to send the corresponding rover state to the db-->
         <form class="content__form contact-form" method="post" action="">
@@ -126,18 +119,18 @@
           <button class="contact-form__button stop" type="submit" onclick="roverStop();" name="stopRover">
             Stop
           </button>
-          <button class="contact-form__button off" type="submit" onclick="roverOff();" name="offRover">
+          <button class="contact-form__button off" type="submit" onclick="roverOff();" name="offover">
             Off
           </button>
 		    <?php
 			if(isset($_POST['startRover'])){
-				$query = mysqli_query($connetion, "INSERT into rover(roverID, status) VALUES (3, 'On')");
+				$query = mysqli_query($connetion, "INSERT into rover(roverID, status) VALUES (2, 'On')");
 			}
 			else if(isset($_POST['stopRover'])){
-				$query = mysqli_query($connetion, "INSERT into rover(roverID, status) VALUES (3, 'Idle')");
+				$query = mysqli_query($connetion, "INSERT into rover(roverID, status) VALUES (2, 'Idle')");
 			}
 			else if(isset($_POST['offRover'])){
-				$query = mysqli_query($connetion, "INSERT into rover(roverID, status) VALUES (3, 'Off')");
+				$query = mysqli_query($connetion, "INSERT into rover(roverID, status) VALUES (2, 'Off')");
 			}
 		   ?>
         </form>
@@ -146,9 +139,9 @@
   </div>
   <!--local storage script of the rover status, to set correctly which buttons are enabled and which not-->
   <script>
-    document.getElementById('rover03status').innerText = window.localStorage.getItem('rover03status');
+    document.getElementById('rover02status').innerText = window.localStorage.getItem('rover02status');
 
-    var status = window.localStorage.getItem('rover03status');
+    var status = window.localStorage.getItem('rover02status');
     if (status == 'Running (Manual)' || status == 'Running (Auto)') {
       document.getElementsByClassName('on').disabled = true;
       document.getElementsByClassName('stop').disabled = false;
@@ -166,7 +159,7 @@
     }
 
     window.addEventListener("storage", function(e) {
-      document.getElementById("rover03status").innerText = window.localStorage.getItem('rover03status');
+      document.getElementById("rover02status").innerText = window.localStorage.getItem('rover02status');
     }, true);
   </script>
   <br /><br />
